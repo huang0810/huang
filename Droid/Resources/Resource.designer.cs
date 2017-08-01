2b57cee2e959144c0dc8f7360d4cf3fe2fba8578
huang0810 <elderhuang@china-airlines.com>
2017-08-01 14:44:09 +0800

    add mywebview

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -50,6 +50,15 @@ namespace huang.Droid
 			// aapt resource value: 0x7f050001
 			public const int myButton = 2131034113;
 			
+			// aapt resource value: 0x7f050002
+			public const int mywebview_btngo = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int mywebview_mywebview = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int mywebview_txturl = 2131034115;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -71,6 +80,9 @@ namespace huang.Droid
 			
 			// aapt resource value: 0x7f030002
 			public const int masterView = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int mywebview = 2130903043;
 			
 			static Layout()
 			{
---
+++ Droid/Webflow/MyWebActivity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Webkit;
+
+namespace huang.Droid.Webflow
+{
+    [Activity(Label = "My Web", MainLauncher =true)]
+    public class MyWebActivity : Activity
+    {
+        private Button btnGOGO { get; set; }
+        private EditText txtUrl { get; set; }
+        private WebView myWebView { get; set; }
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.mywebview);
+
+            btnGOGO = FindViewById<Button>(Resource.Id.mywebview_btngo);
+            txtUrl = FindViewById<EditText>(Resource.Id.mywebview_txturl);
+            myWebView = FindViewById<WebView>(Resource.Id.mywebview_mywebview);
+
+            
+
+            btnGOGO.Click += (sender, e) =>
+            {
+                var urlString = txtUrl.Text;
+                ShowAlert("Alert", urlString, (object alertsender, DialogClickEventArgs alerte) => { });
+                };
+
+        }
+
+        private void ShowAlert(string title, string message, EventHandler<Android.Content.DialogClickEventArgs> positiveButtonClickHandle)
+ 		{ 
+ 
+ 
+ 			AlertDialog.Builder alert = new AlertDialog.Builder(this); 
+ 
+ 
+ 			alert.SetTitle(title); 
+ 			alert.SetMessage(message); 
+ 
+ 
+ 			alert.SetPositiveButton("Confirm", positiveButtonClickHandle); 
+ 
+ 
+ 			RunOnUiThread(() => 
+ 			{
+                alert.Show();
+            }); 
+ 
+ 
+ 		}
+
+
+}
+}
